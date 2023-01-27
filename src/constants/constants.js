38c062d171ceeaefd1617893c347ee0eeecd6e35
dukuaris <dukuaris@gmail.com>
2023-01-27 12:41:42 +0900

    Sorting the project in reverse order

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,83 +1,13 @@
 export const projects = [
 	{
-		title: 'Medium Clone',
+		title: 'Yaongbot',
 		description:
-			'Using Next.js TypeScript, Sanity CMS, React, Tailwind CSS, I have built a Medium clone. I used use Next.js Incremental Static Regeneration to speed up page loading and to optimize the caching of pages. Sanity CMS(Content Management System) as the backend was used to manage structured data.',
-		image: '/images/medium-clone.png',
-		tags: ['Typescript', 'NextJS', 'Sanity CMS'],
-		source: 'https://github.com/dukuaris/dkyoutube',
-		visit: 'https://dkyoutube-dukuaris.vercel.app/',
-		id: 0,
-	},
-	{
-		title: 'Reddit Clone',
-		description:
-			'This is a reddit clone. It provide a sign-in through reddit auth, posting with a subreddit category, up and down voting by clicking the arrows. It utilizes GraphQL which helps efficient operations of database.',
-		image: '/images/reddit-thumbnail.png',
-		tags: ['NextJS', 'GraphQL'],
-		source: 'https://github.com/dukuaris/reddit-clone',
-		visit: 'https://reddit-clone-chi-bice.vercel.app/',
-		id: 1,
-	},
-	{
-		title: 'Spotify Clone',
-		description:
-			'This is a Spotify Clone. This will show an exactly same look and functions as spotify. You can login through your spotify account,see your playlist, and play songs in the list. This app utilize Nextjs Middleware, NextAuth, Recoil for state management and TailwindCss.',
-		image: '/images/spotify-clone-project.png',
-		tags: ['NextJS', 'NextAuth', 'Recoil'],
-		source: 'https://github.com/dukuaris/spotify-clone',
-		visit: 'https://spotify-clone-coral-three.vercel.app/',
-		id: 2,
-	},
-	{
-		title: 'Hulu Clone',
-		description:
-			'This is a Hulu Clone. This will present the movie information from TMDB just like HULU. You can search top 20 movies and dramas by category. You will find specific ways to present contents in grid formats. This app utilize Nextjs, TMDB api and TailwindCss.',
-		image: '/images/hulu-thumbnail.png',
-		tags: ['NextJS', 'TailwindCSS'],
-		source: 'https://github.com/dukuaris/hulu-clone',
-		visit: 'https://hulu-clone-bay-five.vercel.app',
-		id: 3,
-	},
-	{
-		title: 'Netflix Clone',
-		description:
-			'This is a Netflix Clone. This will present the movie information from TMDB just like Netflix. You preview top 20 movies and dramas by category. You will find specific ways to present contents in just the sam format as Netflix. This app utilize Nextjs, Recoil, and Firebase',
-		image: '/images/netflix_thumbnail.png',
-		tags: ['NextJS', 'Recoil', 'Firebase'],
-		source: 'https://github.com/dukuaris/netflix-redesign',
-		visit: 'https://netflix-redesign-psi.vercel.app/',
-		id: 4,
-	},
-	{
-		title: 'Apple Redesign',
-		description:
-			'This is a redesigned apple website. This is a fully functional ecommerce site from login, select products into the cart, and pay through Stripe. Using sanity, the administrator can register products and categories. Users can log in with their own google email accounts',
-		image: '/images/apple-redesign.png',
-		tags: ['NextJS', 'Typescript', 'Sanity', 'Stripe'],
-		source: 'https://github.com/dukuaris/apple-clone',
-		visit: 'https://apple-redesign-eosin.vercel.app/',
-		id: 5,
-	},
-	{
-		title: 'Video Social App',
-		description:
-			"This is a video social app. A user can upload and manage any videos by category. Also a user can review the other's video, give a like, and make a comment on them. This works as a video based social app.",
-		image: '/images/video_app.png',
-		tags: ['NextJS', 'Typescript', 'Sanity', 'Justand'],
-		source: 'https://github.com/dukuaris/tiktik-app',
-		visit: 'https://tiktik-app-two.vercel.app/',
-		id: 6,
-	},
-	{
-		title: 'AI Image Generator',
-		description:
-			'This is AI-powered image generator. Using DALL•E api of OpenAI, this app automatically generates images based on the context of whatever text a user passes in. It creates a different image each time the user enters',
-		image: '/images/image-gen.png',
-		tags: ['NodeJS', 'Express', 'DALL•E'],
-		source: 'https://github.com/dukuaris/ai-image-gen',
-		visit: 'https://ai-image-gen-hazel.vercel.app/',
-		id: 7,
+			"Built on Chatgpt and Dall-e, this chatbot can give not only text answers but also pictures for user's prompts. Also using Google's translator, Korean users can received better results.",
+		image: '/images/yaongbot.png',
+		tags: ['Nodejs', 'Express', 'OpenAI'],
+		source: 'https://github.com/dukuaris/yaongbot',
+		visit: 'https://yaongbot.vercel.app/',
+		id: 9,
 	},
 	{
 		title: 'Yaong Gallery',
@@ -90,14 +20,84 @@ export const projects = [
 		id: 8,
 	},
 	{
-		title: 'Yaongbot',
+		title: 'AI Image Generator',
 		description:
-			"Built on Chatgpt and Dall-e, this chatbot can give not only text answers but also pictures for user's prompts. Also using Google's translator, Korean users can received better results.",
-		image: '/images/yaongbot.png',
-		tags: ['Nodejs', 'Express', 'OpenAI'],
-		source: 'https://github.com/dukuaris/yaongbot',
-		visit: 'https://yaongbot.vercel.app/',
-		id: 9,
+			'This is AI-powered image generator. Using DALL•E api of OpenAI, this app automatically generates images based on the context of whatever text a user passes in. It creates a different image each time the user enters',
+		image: '/images/image-gen.png',
+		tags: ['NodeJS', 'Express', 'DALL•E'],
+		source: 'https://github.com/dukuaris/ai-image-gen',
+		visit: 'https://ai-image-gen-hazel.vercel.app/',
+		id: 7,
+	},
+	{
+		title: 'Video Social App',
+		description:
+			"This is a video social app. A user can upload and manage any videos by category. Also a user can review the other's video, give a like, and make a comment on them. This works as a video based social app.",
+		image: '/images/video_app.png',
+		tags: ['NextJS', 'Typescript', 'Sanity', 'Justand'],
+		source: 'https://github.com/dukuaris/tiktik-app',
+		visit: 'https://tiktik-app-two.vercel.app/',
+		id: 6,
+	},
+	{
+		title: 'Apple Redesign',
+		description:
+			'This is a redesigned apple website. This is a fully functional ecommerce site from login, select products into the cart, and pay through Stripe. Using sanity, the administrator can register products and categories. Users can log in with their own google email accounts',
+		image: '/images/apple-redesign.png',
+		tags: ['NextJS', 'Typescript', 'Sanity', 'Stripe'],
+		source: 'https://github.com/dukuaris/apple-clone',
+		visit: 'https://apple-redesign-eosin.vercel.app/',
+		id: 5,
+	},
+	{
+		title: 'Netflix Clone',
+		description:
+			'This is a Netflix Clone. This will present the movie information from TMDB just like Netflix. You preview top 20 movies and dramas by category. You will find specific ways to present contents in just the sam format as Netflix. This app utilize Nextjs, Recoil, and Firebase',
+		image: '/images/netflix_thumbnail.png',
+		tags: ['NextJS', 'Recoil', 'Firebase'],
+		source: 'https://github.com/dukuaris/netflix-redesign',
+		visit: 'https://netflix-redesign-psi.vercel.app/',
+		id: 4,
+	},
+	{
+		title: 'Hulu Clone',
+		description:
+			'This is a Hulu Clone. This will present the movie information from TMDB just like HULU. You can search top 20 movies and dramas by category. You will find specific ways to present contents in grid formats. This app utilize Nextjs, TMDB api and TailwindCss.',
+		image: '/images/hulu-thumbnail.png',
+		tags: ['NextJS', 'TailwindCSS'],
+		source: 'https://github.com/dukuaris/hulu-clone',
+		visit: 'https://hulu-clone-bay-five.vercel.app',
+		id: 3,
+	},
+	{
+		title: 'Spotify Clone',
+		description:
+			'This is a Spotify Clone. This will show an exactly same look and functions as spotify. You can login through your spotify account,see your playlist, and play songs in the list. This app utilize Nextjs Middleware, NextAuth, Recoil for state management and TailwindCss.',
+		image: '/images/spotify-clone-project.png',
+		tags: ['NextJS', 'NextAuth', 'Recoil'],
+		source: 'https://github.com/dukuaris/spotify-clone',
+		visit: 'https://spotify-clone-coral-three.vercel.app/',
+		id: 2,
+	},
+	{
+		title: 'Reddit Clone',
+		description:
+			'This is a reddit clone. It provide a sign-in through reddit auth, posting with a subreddit category, up and down voting by clicking the arrows. It utilizes GraphQL which helps efficient operations of database.',
+		image: '/images/reddit-thumbnail.png',
+		tags: ['NextJS', 'GraphQL'],
+		source: 'https://github.com/dukuaris/reddit-clone',
+		visit: 'https://reddit-clone-chi-bice.vercel.app/',
+		id: 1,
+	},
+	{
+		title: 'Medium Clone',
+		description:
+			'Using Next.js TypeScript, Sanity CMS, React, Tailwind CSS, I have built a Medium clone. I used use Next.js Incremental Static Regeneration to speed up page loading and to optimize the caching of pages. Sanity CMS(Content Management System) as the backend was used to manage structured data.',
+		image: '/images/medium-clone.png',
+		tags: ['Typescript', 'NextJS', 'Sanity CMS'],
+		source: 'https://github.com/dukuaris/dkyoutube',
+		visit: 'https://dkyoutube-dukuaris.vercel.app/',
+		id: 0,
 	},
 ]
 
